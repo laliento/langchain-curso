df0646a7782a4f33bf0a60d88f81fcab51d9c171
laliento <eduardo.cz.mac@gmail.com>
2026-04-21 14:12:10 -0600

    init helpdesk_system with rag working running setup_rag.py

---
+++ helpdesk_system/config.py
@@ -0,0 +1,5 @@
+CHROMADB_PATH = "C:\\Users\\laptop\\git\\langchain-curso\\helpdesk_system\\chroma_db"
+DOCS_PATH = "C:\\Users\\laptop\\git\\langchain-curso\\helpdesk_system\\docs"
+EMBEDDING_MODEL_PATH = "C:\\Users\\laptop\\git\\langchain-curso\\embeddings\\all-MiniLM-L6-v2"
+
+#EMBEDDINGS_MODEL = "text-embedding-3-large"
