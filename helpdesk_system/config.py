42b430cd6c542cf596c3589edad32e7eb2c3acff
laliento <eduardo.cz.mac@gmail.com>
2026-04-21 15:33:22 -0600

    working helpdesk_system\rag_system.py with main method to test it

--- helpdesk_system/config.py
+++ helpdesk_system/config.py
@@ -1,5 +1,5 @@
 CHROMADB_PATH = "C:\\Users\\laptop\\git\\langchain-curso\\helpdesk_system\\chroma_db"
 DOCS_PATH = "C:\\Users\\laptop\\git\\langchain-curso\\helpdesk_system\\docs"
 EMBEDDING_MODEL_PATH = "C:\\Users\\laptop\\git\\langchain-curso\\embeddings\\all-MiniLM-L6-v2"
-
+LLM_MODEL_PATH = "C:\\Users\\laptop\\git\\langchain-curso\\models"
 #EMBEDDINGS_MODEL = "text-embedding-3-large"
